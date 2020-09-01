c3dac5efb903796efc456eb1920fd2b908c54dd6
adamkimpton <ageofkimptown@gmail.com>
2020-09-01 14:50:10 +1200

    RAD-110: Fixed formatting issues

--- src/main/groovy/com/brightsparklabs/gradle/baseline/BaselinePlugin.groovy
+++ src/main/groovy/com/brightsparklabs/gradle/baseline/BaselinePlugin.groovy
@@ -43,7 +43,7 @@ public class BaselinePlugin implements Plugin<Project> {
          incompatible with loading the dependency via `afterEvaluate`.
          Therefore we disable this plugin from being loaded *specifically* in the test case.
          */
-        if (!project.getName().equals(TEST_PROJECT_NAME)) {
+        if (! project.getName().equals(TEST_PROJECT_NAME)) {
             setupCodeQuality(project)
         }
     }
